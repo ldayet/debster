8e8195d3cc12da3431a11e1d52d9467879fc0455
EL FAIZ MOHAMED <melfaiz@enseirb-matmeca.fr>
2019-04-21 00:33:50 +0200

    Fixed the dashboard
    Added the content class to the css file to use flexbox.
    Added the app name DEBSTER in the navbar.
    Changed the font.

---
+++ includes/footer.php
@@ -0,0 +1,28 @@
+
+      <!-- Colored FAB button with ripple -->
+
+      
+<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--raised mdl-js-ripple-effect mdl-button--colored fixed">
+    <i class="material-icons">add</i>
+    
+</button>
+
+</div>
+
+<!--
+<footer class="mdl-mini-footer">
+  <div class="mdl-mini-footer__left-section">
+    <ul class="mdl-mini-footer__link-list">
+      <li><a href="#">Help</a></li>
+      <li><a href="#">Privacy & Terms</a></li>
+    </ul>
+  </div>
+</footer>
+-->
+
+</main>
+
+
+    <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,5 +1,48 @@
-<?php
-include("includes/header.php"); 
-?>
-  </body>
-</html>
+<?php include("includes/header.php"); ?>
+
+
+<div class="contents">
+
+  <div class="content mdl-color--white mdl-shadow--2dp ">
+      <h2>Inviter un ami</h2>
+
+      <form action="#" class="">
+        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+          <input class="mdl-textfield__input" type="text" id="sample3">
+          <label class="mdl-textfield__label" for="sample3">Saisir une adresse email</label>
+        </div>
+      </form>
+  </div>
+
+
+  <div class="content mdl-color--white mdl-shadow--2dp ">
+      <h2>Votre solde</h2>
+      <h4 class="solde">$57.67</h4>
+  </div>
+
+</div>
+
+<div class="contents">
+<div class="content mdl-color--white mdl-shadow--2dp ">
+      <h2>Toutes les dépenses</h2>
+
+      <ul>
+      <li>
+          <p>3/21/2019 | Vous avez payé 20$ </p>
+        </li>
+        <li>
+        <p>3/21/2019 | Vous avez payé 20$ </p>
+        </li>
+        <li>
+        <p>3/21/2019 | Vous avez payé 20$ </p>
+        </li>
+      </ul>
+  </div>
+
+</div>
+
+
+
+
+
+<?php include("includes/footer.php"); ?>
