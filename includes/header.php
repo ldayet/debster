2d279fd6665da19c3866e861e4fa06dd7def8339
EL FAIZ MOHAMED <melfaiz@enseirb-matmeca.fr>
2019-04-21 12:32:11 +0200

    favicon link in index

--- includes/header.php
+++ includes/header.php
@@ -11,7 +11,7 @@
     <meta name="mobile-web-app-capable" content="yes">
 
 
-    <link rel="shortcut icon" href="images/favicon.png">
+    <link rel="icon" href="images/favicon.ico" />
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
