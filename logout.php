ab44784beb83254c447faf9ea1a4d87d78fab48d
sdarbane <47564609+sdarbane@users.noreply.github.com>
2019-04-23 14:44:45 +0200

    fichier logout.php
    #déconnexion de sa page personnelle

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php session_start() ; ?>
+<?php
+
+if (isset($_SESSION['user'])) {
+    unset($_SESSION['user']);
+    $_SESSION['flash']['success'] = "Vous etes maintenant déconnecté." ;
+    header('Location: login.php');
+}else{
+    header('Location: login.php');
+}
+
+
+?>
